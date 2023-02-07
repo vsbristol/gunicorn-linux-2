d5a86f35e7095b2797ffcadfefe54827998ab8ee
vsbristol <124704669+vsbristol@users.noreply.github.com>
2023-02-07 08:50:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 cors = CORS(app)
 
 def dbCall():
-    time.sleep(3)
     productsdb = [
         { 'id': 0, 'title': 'Apples', 'price': 1.20 },
         { 'id': 1, 'title': 'Bananas', 'price': 1.45 },
